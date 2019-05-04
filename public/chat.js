84df6d2be8432717d8d80794d8a45b873920076d
fed-gren <deveb22@gmail.com>
2019-05-05 01:27:27 +0900

    feat: create a back button
    - 채팅 화면에서 로그인  화면으로  이동하기  위한 back 버튼 생성
    - 로그인 화면에선 login 버튼을 누르면 채팅 화면으로 이동

---
+++ public/chat.js
@@ -0,0 +1,5 @@
+const back_btn = document.getElementById('back');
+
+back_btn.addEventListener('click', () => {
+  location.replace("/");
+});
---
+++ public/index.js
@@ -0,0 +1,5 @@
+const login_btn = document.getElementById("login_submit");
+
+login_btn.addEventListener('click', () => {
+  location.replace("/chat");
+})
